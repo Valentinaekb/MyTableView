3b9309aec99a3947714906a4c7a254889ae2edca
Valentina <v.poleshchuk85@gmail.com>
2021-02-17 21:38:30 +0500

    App Icon Launch Screen Row Height

--- MyTableView/ViewController.swift
+++ MyTableView/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 
     }
-
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return restaurentNames.count
     }
