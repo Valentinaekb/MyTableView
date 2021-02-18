f43a536cb6269d95a8ab0ba8e6a2f5a77d1e8ccc
Valentina <v.poleshchuk85@gmail.com>
2021-02-18 22:19:46 +0500

    Castom Table View Cell

---
+++ MyTableView/CastomeTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CastomeTableViewCell.swift
+//  MyTableView
+//
+//  Created by Валентина on 18.02.2021.
+//
+
+import Foundation
+import UIKit
+
+class CastomeTableViewCell: UITableViewCell{
+    
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
--- MyTableView/ViewController.swift
+++ MyTableView/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
    
     var restaurentNames = ["Burger", "Kitchen", "Bonsai", "Дастархан", "Индокитай","Балкан Гриль","Шерлок Холмс","Speak Easy","Morris Pub","Вкусные истории","Классик","Love&Life","Шок","Бочка"]
     
+    var locationName = ["street1", "street2", "street3", "street4", "street5", "street6", "street7", "street8", "street9", "street10", "street11", "street12", "street13", "street14"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +24,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = restaurentNames[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CastomeTableViewCell
         
-        cell?.imageView?.image = UIImage(named: restaurentNames[indexPath.row])
-        cell?.imageView?.layer.cornerRadius = cell!.frame.size.height / 2
-        cell?.imageView?.clipsToBounds = true
+        cell.nameLabel.text = restaurentNames[indexPath.row]
+        cell.locationLabel.text = locationName[indexPath.row]
+
+        cell.imageOfPlace.image = UIImage(named: restaurentNames[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds = true
     
-        return cell!
+        return cell
         
     }
     
